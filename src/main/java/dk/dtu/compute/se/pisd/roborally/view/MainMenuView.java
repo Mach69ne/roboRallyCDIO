e797fd1860c07f6ff83a5a1cf69f051d50b79e35
Elias Larsen <s235125@dtu.dk>
2024-03-15 01:31:36 +0100

    Created Main Menu

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/MainMenuView.java
@@ -0,0 +1,28 @@
+package dk.dtu.compute.se.pisd.roborally.view;
+
+import dk.dtu.compute.se.pisd.roborally.controller.AppController;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class MainMenuView extends VBox
+{
+
+    public MainMenuView(AppController appController)
+    {
+        Button newGameButton = new Button();
+        newGameButton.setText("New Game");
+        this.getChildren().add(newGameButton);
+        newGameButton.setOnAction(e -> appController.newGame());
+
+        Button loadGameButton = new Button();
+        loadGameButton.setText("Load Game");
+        this.getChildren().add(loadGameButton);
+        loadGameButton.setOnAction(e -> appController.loadGame());
+
+        Button exitGameButton = new Button();
+        exitGameButton.setText("Exit Game");
+        this.getChildren().add(exitGameButton);
+        exitGameButton.setOnAction(e -> appController.exit());
+
+    }
+}
