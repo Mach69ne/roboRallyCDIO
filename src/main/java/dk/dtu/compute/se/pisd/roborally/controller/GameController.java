08d8973dc350abc9efe654134f1ebe1329758f08
Elias Larsen <s235125@dtu.dk>
2024-04-01 00:57:14 +0200

    Removed unnecessary check

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -161,7 +161,7 @@ public class GameController
     // XXX: implemented in the current version
     private void executeCommand(@NotNull Player player, Command command)
     {
-        if (player != null && player.board == board && command != null)
+        if (player.board == board && command != null)
         {
             // XXX This is a very simplistic way of dealing with some basic cards and
             //     their execution. This should eventually be done in a more elegant way
