68202d2ff2188e65a087cb18ccc55eeb4b52cc67
Elias Larsen <s235125@dtu.dk>
2024-03-31 22:32:37 +0200

    Execute current Register now calls the proper function

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/PlayerView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/PlayerView.java
@@ -97,7 +97,7 @@ public class PlayerView extends Tab implements ViewObserver {
         executeButton.setOnAction( e-> gameController.executePrograms());
 
         stepButton = new Button("Execute Current Register");
-        stepButton.setOnAction( e-> gameController.continuePrograms());
+        stepButton.setOnAction( e-> gameController.executeStep());
 
         buttonPanel = new VBox(finishButton, executeButton, stepButton);
         buttonPanel.setAlignment(Pos.CENTER_LEFT);
