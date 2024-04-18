67b4b9819ae4423d163e59475ea5e5e7d4007e90
Elias Larsen <s235125@dtu.dk>
2024-04-18 15:15:27 +0200

    BoardElement has activate method, and boolean if walkable

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
@@ -2,8 +2,19 @@ package dk.dtu.compute.se.pisd.roborally.model;
 
 public class BoardElement
 {
-    BoardElement()
+    private boolean isWalkable;
+    BoardElement(boolean isWalkable)
     {
+        this.isWalkable = isWalkable;
+    }
 
+    public boolean getIsWalkable()
+    {
+        return isWalkable;
+    }
+
+    public void activate()
+    {
+        //
     }
 }
