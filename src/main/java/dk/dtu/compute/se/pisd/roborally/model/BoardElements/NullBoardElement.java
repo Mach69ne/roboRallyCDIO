3dc4eb5ccf9169ea0cc8907cd825f6dd950098bf
Elias Larsen <s235125@dtu.dk>
2024-04-18 20:42:20 +0200

    Created NullBoardElement

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/NullBoardElement.java
@@ -0,0 +1,23 @@
+package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
+
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+
+public class NullBoardElement extends BoardElement
+{
+    NullBoardElement()
+    {
+        super(Heading.NORTH,true);
+    }
+
+    @Override
+    public boolean getCanWalkInto(Heading heading)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean getCanWalkOutOf(Heading heading)
+    {
+        return true;
+    }
+}
