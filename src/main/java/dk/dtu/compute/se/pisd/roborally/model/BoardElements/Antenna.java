860d562612f54d34010dfe548df0a1a429da7050
Elias Larsen <s235125@dtu.dk>
2024-04-19 19:58:44 +0200

    Created Antenna, still missing functionality

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Antenna.java
@@ -0,0 +1,17 @@
+package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+
+public class Antenna extends NullBoardElement
+{
+    public Antenna()
+    {
+        super(true);
+    }
+
+    @Override
+    public void activate(Board board)
+    {
+        //TODO Implement antenna
+    }
+}
