eb2223bd1d6f69181fcc99b30c81be75e770d00b
Elias Larsen <s235125@dtu.dk>
2024-04-19 18:53:54 +0200

    Removed unused methods from Checkpoint

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Checkpoint.java
@@ -5,25 +5,9 @@ import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class Checkpoint extends BoardElement {
-    private Space space;
-    private int checkpointNumber;
 
-    public Checkpoint(Heading heading, boolean isWalkable,Space space, int checkpointNumber) {
+    public Checkpoint(Heading heading, boolean isWalkable) {
         super(heading, isWalkable);
-        this.space = space;
-        this.checkpointNumber = checkpointNumber;
-    }
-
-    public Space getSpace() {
-        return space;
-    }
-
-    public int getCheckpointNumber() {
-        return checkpointNumber;
-    }
-    @Override
-    public void activate() {
-        // TODO implement this
     }
 
     @Override
