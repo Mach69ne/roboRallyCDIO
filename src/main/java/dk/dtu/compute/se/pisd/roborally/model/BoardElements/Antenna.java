86c68dabb8e3844d901cab4bd0b2270911b3731c
Elias Larsen <s235125@dtu.dk>
2024-04-22 08:49:00 +0200

    Antenna now adds itself to Board

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Antenna.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Antenna.java
@@ -1,5 +1,6 @@
 package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
 
+import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class Antenna extends NullBoardElement
@@ -7,6 +8,7 @@ public class Antenna extends NullBoardElement
     public Antenna(Space space)
     {
         super(space);
+        space.board.addBoardElement(Board.ANTENNA_INDEX, this);
     }
 
     @Override
