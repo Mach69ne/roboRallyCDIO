2bf81e55626ab7902f450f7d8f991e756307a43a
Elias Larsen <s235125@dtu.dk>
2024-04-23 21:47:38 +0200

    Added green and blue conveyors

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Conveyors/Conveyor.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Conveyors/Conveyor.java
@@ -1,5 +1,6 @@
-package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
+package dk.dtu.compute.se.pisd.roborally.model.BoardElements.Conveyors;
 
+import dk.dtu.compute.se.pisd.roborally.model.BoardElements.NullBoardElement;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Conveyors/GreenConveyor.java
@@ -0,0 +1,14 @@
+package dk.dtu.compute.se.pisd.roborally.model.BoardElements.Conveyors;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class GreenConveyor extends Conveyor
+{
+    GreenConveyor(Heading orientation, Space space)
+    {
+        super(orientation, 1, space);
+        space.board.addBoardElement(Board.GREEN_CONVEYOR_INDEX, this);
+    }
+}
