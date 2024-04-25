e3f285d3fb7bed11a52d0a65fce630f1abdefca9
Frederik Bastrup <s235117@dtu.dk>
2024-04-25 16:04:44 +0200

    Made damageCards extend commandCards

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/DamageCards/Spam.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/DamageCards/Spam.java
@@ -1,5 +1,15 @@
 package dk.dtu.compute.se.pisd.roborally.model.Cards.DamageCards;
 
-public class Spam {
+import dk.dtu.compute.se.pisd.roborally.model.Command;
+import dk.dtu.compute.se.pisd.roborally.model.CommandCard;
+import org.jetbrains.annotations.NotNull;
 
+public class Spam extends CommandCard {
+
+    /**
+     * @param command the command represented by this card
+     */
+    public Spam(@NotNull Command command) {
+        super(command);
+    }
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/DamageCards/TrojanHorse.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Cards/DamageCards/TrojanHorse.java
@@ -1,4 +1,14 @@
 package dk.dtu.compute.se.pisd.roborally.model.Cards.DamageCards;
 
-public class TrojanHorse {
+import dk.dtu.compute.se.pisd.roborally.model.Command;
+import dk.dtu.compute.se.pisd.roborally.model.CommandCard;
+import org.jetbrains.annotations.NotNull;
+
+public class TrojanHorse extends CommandCard {
+    /**
+     * @param command the command represented by this card
+     */
+    public TrojanHorse(@NotNull Command command) {
+        super(command);
+    }
 }
