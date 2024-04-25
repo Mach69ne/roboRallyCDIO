18440d5957725984c000fea9a0d34d0aecf29a20
Elias Larsen <s235125@dtu.dk>
2024-04-25 16:15:22 +0200

    Autoformatting

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/BoardLaser.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/BoardLaser.java
@@ -1,20 +1,21 @@
 package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
 
 import dk.dtu.compute.se.pisd.roborally.model.Board;
-import dk.dtu.compute.se.pisd.roborally.model.Player;
-import dk.dtu.compute.se.pisd.roborally.model.Position;
-import dk.dtu.compute.se.pisd.roborally.model.Space;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
 
-public class BoardLaser extends BoardElement{
+public class BoardLaser extends BoardElement
+{
 
-   public BoardLaser (Space space, Heading heading){
+    public BoardLaser(Space space, Heading heading)
+    {
         super(heading, true, space);
         space.board.addBoardElement(Board.BOARD_LASER_INDEX, this);
-   }
+    }
 
-   @Override
-    public void activate(){
+    @Override
+    public void activate()
+    {
 
         Heading heading = this.getHeading().next().next();
         this.getSpace().board.getNeighbour(this.getSpace(), heading);
--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Gear.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Gear.java
@@ -2,18 +2,24 @@ package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
 
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
-public class Gear extends NullBoardElement {
+public class Gear extends NullBoardElement
+{
     private final boolean isClockwise;
 
-    public Gear(boolean isWalkable, Space space, boolean isClockwise) {
+    public Gear(boolean isWalkable, Space space, boolean isClockwise)
+    {
         super(isWalkable, space);
         this.isClockwise = isClockwise;
     }
 
-    public void activate() {
-        if (isClockwise) {
+    public void activate()
+    {
+        if (isClockwise)
+        {
             getSpace().getPlayer().setHeading(getSpace().getPlayer().getHeading().next());
-        } else {
+        }
+        else
+        {
             getSpace().getPlayer().setHeading(getSpace().getPlayer().getHeading().prev());
         }
     }
