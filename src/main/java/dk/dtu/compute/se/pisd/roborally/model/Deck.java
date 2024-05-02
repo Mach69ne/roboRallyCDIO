5927181dcc63f8914d0077a5f25c2921c3b1aef9
Elias Larsen <s235125@dtu.dk>
2024-05-02 20:20:36 +0200

    Created Deck class

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Deck.java
@@ -0,0 +1,39 @@
+package dk.dtu.compute.se.pisd.roborally.model;
+
+import java.util.ArrayList;
+
+public class Deck
+{
+    public final ArrayList<Card> playerCards = new ArrayList<>();
+
+    public Deck()
+    {
+        //TODO Add different cards for a player to start with.
+    }
+
+    public void shuffleDeckIntoAnotherDeck(Deck otherDeck)
+    {
+        this.playerCards.addAll(otherDeck.playerCards);
+        this.shuffleDeck();
+        otherDeck.playerCards.clear();
+    }
+
+    public void shuffleDeck()
+    {
+        for (int i = 0; i < playerCards.size(); i++)
+        {
+            int randomIndex = (int) (Math.random() * playerCards.size());
+            Card tempCard = playerCards.get(randomIndex);
+            playerCards.set(randomIndex, playerCards.get(i));
+            playerCards.set(i, tempCard);
+        }
+    }
+
+    public Card drawTopCard()
+    {
+        Card tempCard = playerCards.get(0);
+        playerCards.remove(0);
+        return tempCard;
+    }
+
+}
