d111918262733599ec453a36b14169d770eb3b34
Frederik Bastrup <s235117@dtu.dk>
2024-05-02 13:36:24 +0200

    Removed unnecessary methods from reboottoken class and fixed spacetemplate

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/RebootToken.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/RebootToken.java
@@ -3,19 +3,9 @@ package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
-public class RebootToken extends BoardElement{
-    private Space space;
+public class RebootToken extends BoardElement {
 
     public RebootToken(Heading heading, boolean isWalkable, Space space) {
         super(heading, isWalkable, space);
     }
-
-    Space getRebootToken() {
-        return this.space;
-    }
-
-    void setRebootToken(Space space) {
-        this.space = space;
-    }
-
 }
