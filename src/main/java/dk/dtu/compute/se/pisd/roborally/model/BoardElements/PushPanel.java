1e53eec4be95d2cb0dc941353beee5d77269ae0c
adel051700 <s235076@dtu.dk>
2024-05-11 23:48:10 +0200

    Cadded setImage in constructor of pushPanel.java. push135.png is changed to have an empty tile and wall.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/PushPanel.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/PushPanel.java
@@ -3,6 +3,7 @@ package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
+import javafx.scene.image.Image;
 
 /**
  * @author Emil
@@ -16,6 +17,7 @@ public class PushPanel extends BoardElement
         super(orientation, true, space);
         this.orientation = orientation;
         space.board.addBoardElement(Board.PUSH_PANELS_INDEX, this);
+        setImage(new Image("file:src/main/resources/images/push135.png"));
     }
 
     /**
