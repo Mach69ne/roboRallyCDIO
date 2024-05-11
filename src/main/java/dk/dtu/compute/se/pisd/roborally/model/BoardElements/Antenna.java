8d95eb8350e81413be26487241838963b99d597e
Elias Larsen <s235125@dtu.dk>
2024-05-11 18:59:47 +0200

    Antenna now has texture

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Antenna.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Antenna.java
@@ -4,6 +4,7 @@ import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 import dk.dtu.compute.se.pisd.roborally.model.Position;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
+import javafx.scene.image.Image;
 
 /**
  * @author
@@ -16,6 +17,7 @@ public class Antenna extends NullBoardElement
     public Antenna(Space space)
     {
         super(false, space);
+        this.setImage(new Image("file:src/main/Resources/Images/antenna.png"));
         space.board.addBoardElement(Board.ANTENNA_INDEX, this);
     }
 
