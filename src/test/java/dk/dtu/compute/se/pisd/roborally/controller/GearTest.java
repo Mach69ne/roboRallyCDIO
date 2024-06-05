7ea3acf2fc29b1114aa9f0a0187d94721b35f2ff
Emil Savino <s235096@dtu.dk>
2024-06-05 14:54:01 +0200

    Created test for gear

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GearTest.java
@@ -0,0 +1,53 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.BoardElements.BoardElement;
+import dk.dtu.compute.se.pisd.roborally.model.BoardElements.Gear;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import javafx.application.Platform;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GearTest {
+    private final int TEST_WIDTH = 8;
+    private final int TEST_HEIGHT = 8;
+
+    private GameController gameController;
+    private MoveController moveController;
+
+    @BeforeEach
+    void setUp() {
+        Platform.startup(() -> {});
+        Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
+        gameController = new GameController(board);
+        moveController = new MoveController(gameController);
+
+        for (int i = 0; i < 2; i++) {
+            Player player = new Player(board, null,"Player " + i, moveController);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(i, i));
+            player.setHeading(Heading.values()[i % Heading.values().length]);
+        }
+
+        new Gear(board.getSpace(0, 1), true);
+        board.setCurrentPlayer(board.getPlayer(0));
+    }
+
+    @AfterEach
+    void tearDown() {
+        gameController = null;
+    }
+
+    @Test
+    void activate() {
+        Board board = gameController.board;
+        BoardElement gear = board.getSpace(0, 1).getBoardElement();
+        Player player = board.getCurrentPlayer();
+        player.moveController.moveForward(player);
+        gear.activate();
+        Assertions.assertEquals(Heading.WEST, player.getHeading());
+    }
+}
