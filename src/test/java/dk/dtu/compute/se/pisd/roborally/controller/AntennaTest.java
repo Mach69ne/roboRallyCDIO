b3f4f9db9ed05b14539799dc6d6cff3a90e3997e
Emil Savino <s235096@dtu.dk>
2024-06-05 13:22:51 +0200

    Started creating test for antenna

--- src/test/java/dk/dtu/compute/se/pisd/roborally/controller/AntennaTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/AntennaTest.java
@@ -1,2 +1,50 @@
-package dk.dtu.compute.se.pisd.roborally.controller;public class AntennaTest {
+package dk.dtu.compute.se.pisd.roborally.controller;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.BoardElements.Antenna;
+import dk.dtu.compute.se.pisd.roborally.model.BoardElements.BoardElement;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import javafx.application.Platform;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AntennaTest {
+    private final int TEST_WIDTH = 8;
+    private final int TEST_HEIGHT = 8;
+
+    private GameController gameController;
+    private MoveController moveController;
+
+    @BeforeEach
+    void setUp() {
+        Platform.startup(() -> {});
+        Board board = new Board(TEST_WIDTH, TEST_HEIGHT);
+        gameController = new GameController(board);
+        moveController = new MoveController(gameController);
+
+        for (int i = 0; i < 2; i++) {
+            Player player = new Player(board, null,"Player " + i, moveController);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(i, 1));
+            player.setHeading(Heading.values()[i % Heading.values().length]);
+        }
+
+        new Antenna(board.getSpace(5, 5));
+    }
+
+    @AfterEach
+    void tearDown() {
+        gameController = null;
+    }
+
+    @Test
+    void activate() {
+        Board board = gameController.board;
+        BoardElement antenna = board.getSpace(5, 5).getBoardElement();
+        antenna.activate();
+        Player player = board.getCurrentPlayer();
+        Assertions.assertEquals(player, board.getPlayer(1));
+    }
 }
