651f54c88a3bd79405eeb321a33527eff3b001ab
Emil Savino <s235096@dtu.dk>
2024-06-05 14:36:53 +0200

    Created test for blue conveyor belt

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Conveyors/BlueConveyor.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Conveyors/BlueConveyor.java
@@ -12,7 +12,7 @@ public class BlueConveyor extends Conveyor
 {
     public BlueConveyor(Heading heading, Space space)
     {
-        super(heading, 3, space);
+        super(heading, 2, space);
         space.board.addBoardElement(Board.BLUE_CONVEYOR_INDEX, this);
         setImage(new Image("file:src/main/resources/images/blue.png"));
     }
