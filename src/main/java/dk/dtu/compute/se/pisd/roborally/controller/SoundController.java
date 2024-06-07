92ab2979774cbf3d38a4e8784a8d84efd26ed63a
adel051700 <s235076@dtu.dk>
2024-06-07 22:20:08 +0200

    added new music, and loopSound method now takes in a array of file names as a parameter, and loops through the array.

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/SoundController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/SoundController.java
@@ -4,30 +4,28 @@ import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.URL;
 
-public class SoundController
-{
+public class SoundController {
     private static SoundController instance;
     private Clip clip;
+    private int currentSoundIndex = 0;
 
-    private SoundController()
-    {
+    private SoundController() {
     }
 
-    public static SoundController getInstance()
-    {
-        if (instance == null)
-        {
+    public static SoundController getInstance() {
+        if (instance == null) {
             instance = new SoundController();
         }
         return instance;
     }
 
-    public void playSound(String soundSrc)
-    {
-        try
-        {
+    public void playSound(String soundSrc) {
+        try {
             // Open an audio input stream.
             URL url = getClass().getResource("/sounds/" + soundSrc + ".wav");
+            if (url == null) {
+                return;
+            }
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
             // Get a sound clip resource.
             clip = AudioSystem.getClip();
@@ -36,59 +34,51 @@ public class SoundController
             FloatControl fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             fc.setValue(-30.00f);
             clip.start();
-        }
-        catch (UnsupportedAudioFileException e)
-        {
+        } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (LineUnavailableException e)
-        {
+        } catch (LineUnavailableException e) {
             e.printStackTrace();
         }
     }
 
-    public void stopSound()
-    {
-        if (clip != null && clip.isRunning())
-        {
+    public void stopSound() {
+        if (clip != null && clip.isRunning()) {
             clip.stop();
         }
     }
 
-    public void loopSound(String soundSrc)
-    {
-        try
-        {
-            URL url = getClass().getResource("/sounds/" + soundSrc + ".wav");
-            if (url == null)
-            {
+    public void loopSound(String[] soundSrcs) {
+        try {
+            // Open an audio input stream.
+            URL url = getClass().getResource("/sounds/" + soundSrcs[currentSoundIndex] + ".wav");
+            if (url == null) {
                 return;
             }
             AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
-
+            // Get a sound clip resource.
             clip = AudioSystem.getClip();
-
+            // Open audio clip and load samples from the audio input stream.
             clip.open(audioIn);
             FloatControl fc = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             fc.setValue(-15.00f);
+            clip.start();
+            clip.addLineListener(e -> {
+                if (!clip.isRunning()) {
+                    currentSoundIndex++;
+                    currentSoundIndex %= soundSrcs.length;
+                    loopSound(soundSrcs); // play the next sound
+                }
+            });
 
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        }
-        catch (UnsupportedAudioFileException e)
-        {
+        } catch (UnsupportedAudioFileException e) {
             e.printStackTrace();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        catch (LineUnavailableException e)
-        {
+        } catch (LineUnavailableException e) {
             e.printStackTrace();
         }
     }
+
 }
