d5e661c1feb0e9e5808258eb17486da12f1f83fd
Mads Hartwich <madshartwich@gmail.com>
2024-06-09 13:35:05 +0200

    Created main menu buttoms with images for  new game, load game, and exit game

--- src/main/java/dk/dtu/compute/se/pisd/roborally/view/MainMenuView.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/view/MainMenuView.java
@@ -5,6 +5,8 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * @author Elias
@@ -26,23 +28,28 @@ public class MainMenuView extends VBox
         Label label = new Label("RoboRally");
         this.getChildren().add(label);
 
-
-        Button newGameButton = new Button();
-        newGameButton.setText("New Game");
+        Button newGameButton = createButton("file:src/main/resources/Images/newGame.png", "New Game");
         this.getChildren().add(newGameButton);
         newGameButton.setOnAction(e -> appController.newGame());
 
-        Button loadGameButton = new Button();
-        loadGameButton.setText("Load Game");
+        Button loadGameButton = createButton("file:src/main/resources/Images/loadGame.png", "Load Game");
         this.getChildren().add(loadGameButton);
         loadGameButton.setOnAction(e -> appController.loadGame());
 
-        Button exitGameButton = new Button();
-        exitGameButton.setText("Exit Game");
+        Button exitGameButton = createButton("file:src/main/resources/Images/exitGame.png", "Exit Game");
         this.getChildren().add(exitGameButton);
         exitGameButton.setOnAction(e -> appController.exit());
 
-        //Set spacing between buttons here
+        // Set spacing between buttons
         this.setSpacing(30);
+    }
+    private Button createButton(String imagePath, String tooltipText) {
+        Button button = new Button();
+        Image image = new Image(imagePath);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(100); // Adjust height as needed
+        imageView.setFitWidth(350);  // Adjust width as needed
+        button.setGraphic(imageView);
+        return button;
     }
 }
